e25334cad8911cb083c667547eff24b8a9a62ec8
marcelorl <marcelo_rl2@hotmail.com>
2024-05-24 00:16:02 -0300

    autocomplete impl

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import './App.css';
 import ErrorBoundary from './components/ErrorBoundary.tsx';
+import AutoComplete from './components/AutoComplete.tsx';
 
 function App() {
   return (
     <ErrorBoundary>
       <div>
         <h1>AutoComplete Component</h1>
+        <AutoComplete />
       </div>
     </ErrorBoundary>
   );
---
+++ src/components/AutoComplete.tsx
@@ -0,0 +1,81 @@
+import React, { useState, useMemo, useCallback, useTransition, useEffect } from 'react';
+
+const AutoComplete: React.FC = () => {
+  const [query, setQuery] = useState('');
+  const [results, setResults] = useState<string[]>([]);
+  const [allData, setAllData] = useState<string[]>([]);
+  const [isPending, startTransition] = useTransition();
+
+  useEffect(() => {
+    fetch('/data.json')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+
+        return response.json();
+      })
+      .then((data: Array<{ email: string }>) => setAllData(data.map((item) => item.email)))
+      .catch((error) => {
+        console.error('Failed to load data', error);
+        throw error;
+      });
+  }, []);
+
+  const handleChange = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      const { value } = event.target;
+      setQuery(value);
+
+      startTransition(() => {
+        if (value.trim() === '') {
+          setResults([]);
+        } else {
+          const filteredResults = [];
+          for (let i = 0; i < allData.length; i++) {
+            const item = allData[i];
+            const isItemFound = item.trim().toLowerCase().startsWith(value.toLowerCase().trim());
+
+            if (isItemFound) {
+              filteredResults.push(item);
+            }
+            if (filteredResults.length >= 10) {
+              break;
+            }
+          }
+
+          setResults(filteredResults);
+        }
+      });
+    },
+    [allData],
+  );
+
+  const handleSelect = useCallback((value: string) => {
+    setQuery(value);
+    setResults([]);
+  }, []);
+
+  const memoizedResults = useMemo(() => {
+    return results.map((result) => (
+      <li key={result} onClick={() => handleSelect(result)}>
+        {result}
+      </li>
+    ));
+  }, [results, handleSelect]);
+
+  const isNotFound = useMemo(() => {
+    return Boolean(query.length && !memoizedResults.length && !allData.find((item) => query === item));
+  }, [allData, memoizedResults.length, query]);
+
+  return (
+    <div>
+      <input type="text" value={query} onChange={handleChange} placeholder="Search..." />
+      {isPending && <div>Loading...</div>}
+      <ul>{memoizedResults}</ul>
+      {isNotFound && <div>No results</div>}
+    </div>
+  );
+};
+
+export default AutoComplete;
